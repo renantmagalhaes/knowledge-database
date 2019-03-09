7d91eb454059422a089d9e72c62c59394934abeb
renantmagalhaes <renantmagalhaes@gmail.com>
2019-03-08 21:15:07 -0300

    aws cli

--- aws-cli/aws-cli.sh
+++ aws-cli/aws-cli.sh
@@ -4,7 +4,7 @@
 curl -O https://bootstrap.pypa.io/get-pip.py
 
 #Install pip - for all users
-python get-pip.py 
+sudo python get-pip.py 
 
 #Install aws cli for all users (dont need to add path)
-pip install awscli
+sudo pip install awscli
