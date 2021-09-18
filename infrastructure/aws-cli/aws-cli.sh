67a37e5a60e52853650d25ba0a4cc204c2cd23c1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2021-09-18 14:26:18 +0200

    aws cli v2

--- infrastructure/aws-cli/aws-cli.sh
+++ infrastructure/aws-cli/aws-cli.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-#Download pip
-curl -O https://bootstrap.pypa.io/get-pip.py
+# OLD WAY
+# #Download pip
+# curl -O https://bootstrap.pypa.io/get-pip.py
 
-#Install pip - for all users
-sudo python get-pip.py 
+# #Install pip - for all users
+# sudo python get-pip.py 
 
-#Install aws cli for all users (dont need to add path)
-sudo pip install awscli
+# #Install aws cli for all users (dont need to add path)
+# sudo pip install awscli
+
+# Install AWS CLI
+cd /tmp/
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
